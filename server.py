a9e8657b3fb5898bb48b5616f463b323f3afc06a
kcgroulx <87883655+kcgroulx@users.noreply.github.com>
2023-12-21 16:03:37 -0800

    created server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def api():
+    data = request.get_json()
+    # Do some processing with the data
+    result = {'result': 'Processed data: ' + data['message']}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(port=5000)
